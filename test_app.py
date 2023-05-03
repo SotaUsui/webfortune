557f79e7039da5db124c0404e5ad885d7599e359
Sota Usui <sota0829clover@gmail.com>
2023-05-03 03:12:10 -0500

    done

--- test_app.py
+++ test_app.py
@@ -10,9 +10,18 @@ def app():
 def client(app):
     yield app.test_client()
 
+
+def test_fortune(app, client):
+    res = client.get('/fortune/')
+    assert res.status_code == 200
+
 def test_cowsay(app, client):
     message = 'hello'
     res = client.get('/cowsay/%s/' % message)
     assert res.status_code == 200
     page_output = res.get_data(as_text=True)
     assert message in page_output
+
+def test_cowfortune(app, client):
+    res = client.get('/cowfortune/')
+    assert res.status_code == 200
